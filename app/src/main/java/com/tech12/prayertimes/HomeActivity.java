5d58b920c654128e13b98cec3260b8d5dddc3eef
Muhammad Hassaan <mohammad.hassaan21@gmail.com>
2018-12-24 02:58:00 +0500

    Navigation Drawer Added

--- app/src/main/java/com/tech12/prayertimes/HomeActivity.java
+++ app/src/main/java/com/tech12/prayertimes/HomeActivity.java
@@ -2,8 +2,13 @@ package com.tech12.prayertimes;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,123 +30,34 @@ import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String TAG = "Log";
-    String url;
-    // Tag used to cancel the request
-    String tag_json_obj = "json_obj_req";
-    ProgressDialog pDialog;
-
-    TextView fajr, dhuhr, asr, maghrib, isha, location, date;
-    EditText search_et;
-    Button search_btn;
-
-
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        fajr = (TextView) findViewById(R.id.fajr);
-        dhuhr = (TextView) findViewById(R.id.dhuhr);
-        asr = (TextView) findViewById(R.id.asr);
-        maghrib = (TextView) findViewById(R.id.maghrib);
-        isha = (TextView) findViewById(R.id.isha);
-        location = (TextView) findViewById(R.id.location);
-        date = (TextView) findViewById(R.id.date);
-        search_et= (EditText) findViewById(R.id.search);
-        search_btn = (Button) findViewById(R.id.search_btn);
+        HomeFragment home =new HomeFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,home).commit();
 
-        search_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String myLocation = search_et.getText().toString().trim();
-                if(myLocation.isEmpty()){
-                    Toast.makeText(HomeActivity.this, "Please enter location", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    url = "http://muslimsalat.com/"+myLocation+".json?key=7f19cfbd644c7f067a758f00b35464df";
-                    searchLocation();
-                }
-            }
-        });
-    }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    private void searchLocation() {
+        drawer = findViewById(R.id.drawer_layout);
 
-        pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
-                url, null,
-                new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            //Get location
-                            String country = response.get("country").toString();
-                            String state = response.get("state").toString();
-                            String city = response.get("city").toString();
-                            String Location = city +", "+state+", "+country;
-
-                            String Date = response.getJSONArray("items").getJSONObject(0).get("date_for").toString();
-
-                            //Get prayer timings
-                            String Fajar = response.getJSONArray("items").getJSONObject(0).get("fajr").toString();
-                            String Dhuhr = response.getJSONArray("items").getJSONObject(0).get("dhuhr").toString();
-                            String Asr = response.getJSONArray("items").getJSONObject(0).get("asr").toString();
-                            String Maghrib = response.getJSONArray("items").getJSONObject(0).get("maghrib").toString();
-                            String Isha = response.getJSONArray("items").getJSONObject(0).get("isha").toString();
-
-                            //Set data to the Textviews
-                            fajr.setText(Fajar);
-                            dhuhr.setText(Dhuhr);
-                            asr.setText(Asr);
-                            maghrib.setText(Maghrib);
-                            isha.setText(Isha);
-                            location.setText(Location);
-                            date.setText(Date);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        pDialog.hide();
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                Toast.makeText(HomeActivity.this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
-                // hide the progress dialog
-                pDialog.hide();
-            }
-        });
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
-
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.duas:
-                Intent intent = new Intent(this, DuasActivity.class);
-                this.startActivity(intent);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
---
+++ app/src/main/java/com/tech12/prayertimes/HomeFragment.java
@@ -0,0 +1,137 @@
+package com.tech12.prayertimes;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    private static final String TAG = "Log";
+    String url;
+    // Tag used to cancel the request
+    String tag_json_obj = "json_obj_req";
+    ProgressDialog pDialog;
+
+    TextView fajr, dhuhr, asr, maghrib, isha, location, date;
+    EditText search_et;
+    Button search_btn;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        fajr = (TextView) view.findViewById(R.id.fajr);
+        dhuhr = (TextView) view.findViewById(R.id.dhuhr);
+        asr = (TextView) view.findViewById(R.id.asr);
+        maghrib = (TextView) view.findViewById(R.id.maghrib);
+        isha = (TextView) view.findViewById(R.id.isha);
+        location = (TextView) view.findViewById(R.id.location);
+        date = (TextView) view.findViewById(R.id.date);
+        search_et = (EditText) view.findViewById(R.id.search);
+        search_btn = (Button) view.findViewById(R.id.search_btn);
+
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myLocation = search_et.getText().toString().trim();
+                if (myLocation.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter location", Toast.LENGTH_SHORT).show();
+                } else {
+                    url = "http://muslimsalat.com/" + myLocation + ".json?key=7f19cfbd644c7f067a758f00b35464df";
+                    searchLocation();
+                }
+            }
+        });
+        return view;
+    }
+
+    private void searchLocation() {
+
+        pDialog = new ProgressDialog(getContext());
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //Get location
+                            String country = response.get("country").toString();
+                            String state = response.get("state").toString();
+                            String city = response.get("city").toString();
+                            String Location = city + ", " + state + ", " + country;
+
+                            String Date = response.getJSONArray("items").getJSONObject(0).get("date_for").toString();
+
+                            //Get prayer timings
+                            String Fajar = response.getJSONArray("items").getJSONObject(0).get("fajr").toString();
+                            String Dhuhr = response.getJSONArray("items").getJSONObject(0).get("dhuhr").toString();
+                            String Asr = response.getJSONArray("items").getJSONObject(0).get("asr").toString();
+                            String Maghrib = response.getJSONArray("items").getJSONObject(0).get("maghrib").toString();
+                            String Isha = response.getJSONArray("items").getJSONObject(0).get("isha").toString();
+
+                            //Set data to the Textviews
+                            fajr.setText(Fajar);
+                            dhuhr.setText(Dhuhr);
+                            asr.setText(Asr);
+                            maghrib.setText(Maghrib);
+                            isha.setText(Isha);
+                            location.setText(Location);
+                            date.setText(Date);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getContext(), "Please check your internet connection", Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                pDialog.hide();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+
+    }
+}
+
