74ff7d848c1805a12258b260cfbf164616f4afd3
Muhammad Hassaan <mohammad.hassaan21@gmail.com>
2018-12-24 19:12:03 +0500

    Fragments Added

--- app/src/main/java/com/tech12/prayertimes/HomeActivity.java
+++ app/src/main/java/com/tech12/prayertimes/HomeActivity.java
@@ -2,6 +2,8 @@ package com.tech12.prayertimes;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,26 +11,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawer;
     @Override
@@ -45,11 +31,36 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.duas:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DuasFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.qibla:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new QiblaFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.rate:
+                Toast.makeText(this, "Rate Our App Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "Logout Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/tech12/prayertimes/QiblaFragment.java
@@ -0,0 +1,29 @@
+package com.tech12.prayertimes;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class QiblaFragment extends Fragment {
+
+
+    public QiblaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_qibla, container, false);
+        return view;
+    }
+
+}
